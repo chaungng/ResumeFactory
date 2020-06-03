70f9d302d8a1d3a06cdf3911eef97f59299466e2
Minh Hong <hellyeyoudo@gmail.com>
2020-06-03 00:56:17 -0700

    http service

---
+++ web/resume-factory-web/src/Services/HttpServices.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+const baseURL = "http://localhost:8080";
+export default class HttpServices extends Component{
+    
+    getAxiosInstance(){
+        return axios.create({
+            baseURL: baseURL,
+            headers: {
+                "Content-type": "application/json"
+            }
+        });
+    }
+
+    axiosInstance = getAxiosInstance();
+
+    getAllData (url){
+        return this.axiosInstance.get('/' + url);
+    } 
+
+    getDataById (url, id){
+        return this.axiosInstance.get('/' + url + '/' + id);
+    }
+
+    create (url, data){
+        return this.axiosInstance.post('/' + url, data);
+    }
+
+    update (url, id, data){
+        return this.axiosInstance.put('/' + url + '/' + id, data);
+    }
+
+    delete (url, id){
+        return this.axiosInstance.delete('/' + url + '/' + id);
+    }
+
+    deleteAll (url){
+        return this.axiosInstance.delete('/' + url);
+    }
+}
