2ca681379019485c8ecf1b70c51cc1cb2bb9daa6
steve <stevennhnk113@gmail.com>
2020-06-14 19:14:54 -0700

    adding connection to backend

--- web/resume-factory-web/src/Services/HttpServices.js
+++ web/resume-factory-web/src/Services/HttpServices.js
@@ -1,19 +1,35 @@
-import React, { Component } from 'react';
 import axios from 'axios';
 
-const baseURL = "http://localhost:8000";
-export default class HttpServices extends Component{
+const baseURL = "http://localhost:8000/api";
+export default class HttpServices{
     
     getAxiosInstance(){
         return axios.create({
             baseURL: baseURL,
             headers: {
-                "Content-type": "application/json"
+                "Content-type": "application/json",
+                "cache-control": "no-cache"
             }
         });
     }
 
-    axiosInstance = getAxiosInstance();
+    axiosInstance = this.getAxiosInstance();
+
+    async makeRequest(func, url, data) {
+        try {
+            return await func(url, data)
+        } catch {
+            return null
+        }
+    }
+
+    async get(url){
+        return await this.makeRequest(this.axiosInstance.get, '/' + url)
+    }
+
+    async post(url, data) {
+        return await this.makeRequest(this.axiosInstance.post, '/' + url, data)
+    }
 
     getAllData (url){
         return this.axiosInstance.get('/' + url);
---
+++ web/resume-factory-web/src/viewModel/userViewModel.js
@@ -0,0 +1,5 @@
+export class UserViewModel {
+    email
+    firstName
+    lastName
+}
