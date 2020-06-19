4b9b8fe35ae8f865df6f5c20eba6378b9c2b87fa
steve <stevennhnk113@gmail.com>
2020-06-18 18:03:20 -0700

    add full docker

--- backend/src/main/java/com/group/resumefactory/app/entities/User.java
+++ backend/src/main/java/com/group/resumefactory/app/entities/User.java
@@ -4,33 +4,26 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
-//@Table(name = "users",uniqueConstraints = {
-//        @UniqueConstraint(columnNames = {
-//                "username"
-//        })
-//})
-//@EntityListeners(AuditingEntityListener.class)
-//@JsonIgnoreProperties(value = {"createdAt", "updatedAt", "Password"},
-//        allowGetters = true)
-@Table (name = "users")
+@Table(name = "users",uniqueConstraints = {
+       @UniqueConstraint(columnNames = {
+               "username"
+       })
+})
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt", "Password"},
+       allowGetters = true)
+// @Table (name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
