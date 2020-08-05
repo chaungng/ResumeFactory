2d1e7636ca14e9a751659d4fcccbe336843b911d
Minh Hong <hellyeyoudo@gmail.com>
2020-08-05 02:14:51 -0700

    fix bug invalid data of bsic info

--- frontend/src/components/NewResumeForm.jsx
+++ frontend/src/components/NewResumeForm.jsx
@@ -15,6 +15,7 @@ import ResumeController from '../controllers/ResumeController';
 // import localForage from "localforage";
 import {DataContext} from "../contexts/DataContext";
 import localForage from "localforage";
+import { Alert } from '@material-ui/lab';
 
 
 class NewResumeForm extends Component {
@@ -27,12 +28,16 @@ class NewResumeForm extends Component {
         educationInfo: null,
         skills: null,
         basicInfo: null,
+        isValid: false,
     };
 
     // user = useContext(DataContext)
     constructor(props) {
         super(props);
         this.resumeService = new ResumeServices();
+        this.state= {
+            isValid: true,
+        }
     }
 
     getPersonalInfo = (childData) => {
@@ -66,26 +71,54 @@ class NewResumeForm extends Component {
         );
     }
 
-    async createNewResume(data) {
-        let resumeData = {
-            userId: this.context.user.userId,
-            content: JSON.stringify(data),
-            title: data.basicInfo.title,
-            level: data.basicInfo.level,
-            company: data.basicInfo.company
-        };
-
-        let response = await ResumeController.addNewResume(resumeData);
-        if (response.id !== null || response.id !== undefined) {
-            //TODO: handle response in case error
-            let count = await ResumeController.getCountResumeByUserId(this.context.user.userId);
-            this.context.user.setNumOfResume(count);
-            await localForage.setItem('numOfResume', count);
-            this.props.history.push('/');
-            // return true;
-        } else {
-            // return false;
+    validate(data){
+        if (data == null){
+            this.state({
+                isValid: false
+            });
+        } else{
+            if (data.basicInfo == null ){
+                this.setState({
+                    isValid: false
+                });
+            } else {
+                this.setState({
+                    isValid: true
+                });
+            }
         }
+    }
+    async createNewResume(data) {
+        this.validate(data);
+        if (data.basicInfo != null){
+            let resumeData = {
+                userId: this.context.user.userId,
+                content: JSON.stringify(data),
+                title: data.basicInfo.title,
+                level: data.basicInfo.level,
+                company: data.basicInfo.company
+            };
+    
+            let response = await ResumeController.addNewResume(resumeData);
+            if (response.id !== null || response.id !== undefined) {
+                //TODO: handle response in case error
+                this.setState({
+                    isValid : true,
+                });
+                let count = await ResumeController.getCountResumeByUserId(this.context.user.userId);
+                this.context.user.setNumOfResume(count);
+                
+                await localForage.setItem('numOfResume', count);
+                this.props.history.push('/');
+                // return true;
+                
+            } else {
+                this.setState({
+                    isValid : false,
+                });
+            }
+        }
+        
     }
 
     async prepareResume() {
@@ -113,11 +146,14 @@ class NewResumeForm extends Component {
                         </Button>
                     </Grid>
                     <Grid item xs={12} sm={1}>
-                        <Button variant="contained" color="primary" onClick={() => this.prepareResume()}>
+                        <Button variant="contained" color="primary" onClick={() => this.prepareResume()} >
                             Save
                         </Button>
                     </Grid>
                 </Grid>
+                {!this.state.isValid ? 
+                        <Alert severity="error">Something wrong! Data is invalid!</Alert>
+                        : ""}
                 <BasicInfoSection set={this.getBasicInfo} isView={false}/>
                 <br/>
                 <PersonalInfoSection set={this.getPersonalInfo} isView={false}/>
@@ -126,7 +162,7 @@ class NewResumeForm extends Component {
                 <br/>
                 <SkillsSection set={this.getSkills} isView={false}/>
                 <br/>
-                <EducationSection set={this.getEducationInfo}/>
+                <EducationSection set={this.getEducationInfo} isView={false}/>
 
             </div>
         </React.Fragment>);
